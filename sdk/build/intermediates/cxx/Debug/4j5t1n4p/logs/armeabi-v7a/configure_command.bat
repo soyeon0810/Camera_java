196f001200127ff8a5626d9e09617f4d77925cc1
joosoyeon <sy0834@naver.com>
2023-02-23 14:38:08 +0900

    commit

---
+++ sdk/build/intermediates/cxx/Debug/4j5t1n4p/logs/armeabi-v7a/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\Users\\jooso\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\jooso\\AndroidStudioProjects\\MyApplication\\sdk\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=armeabi-v7a" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a" ^
+  "-DANDROID_NDK=C:\\Users\\jooso\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\jooso\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\jooso\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\jooso\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\jooso\\AndroidStudioProjects\\MyApplication\\sdk\\build\\intermediates\\cxx\\Debug\\4j5t1n4p\\obj\\armeabi-v7a" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\jooso\\AndroidStudioProjects\\MyApplication\\sdk\\build\\intermediates\\cxx\\Debug\\4j5t1n4p\\obj\\armeabi-v7a" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BC:\\Users\\jooso\\AndroidStudioProjects\\MyApplication\\sdk\\.cxx\\Debug\\4j5t1n4p\\armeabi-v7a" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
